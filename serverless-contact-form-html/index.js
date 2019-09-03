62630f534f9d8a0cad42ef4091964d8c8ae2f14a
Zachariah Crowell <zachariah.crowell@techtonic.com>
2019-09-03 09:36:33 -0600

    Added initial files

---
+++ serverless-contact-form-html/index.js
@@ -0,0 +1,34 @@
+
+
+(() => {
+
+    const form = document.querySelector('form');
+    const formResponse = document.querySelector('js-form-response');
+  
+    form.onsubmit = e => {
+      e.preventDefault();
+  
+      const data = {};
+
+      const formElements = Array.from(form);
+
+      formElements.map(input => {
+          input.value !== '' ? data[input.name] = input.value : false
+      });
+  
+      // Log what our lambda function will receive
+      console.log(JSON.stringify(data));
+
+      fetch('https://dwxt1f2tse.execute-api.us-east-1.amazonaws.com/dev/static-site-mailer', {
+        method: 'POST',
+        body: JSON.stringify(data)
+      }).then(response => {
+        return response.json();
+      }).then(data => {
+        console.log(data);
+      })
+
+    };
+  })();
+
+  // "User `arn:aws:sts::801906911770:assumed-role/static-site-mailer-dev-us-east-1-lambdaRole/static-site-mailer-dev-staticSiteMailer' is not authorized to perform `ses:SendEmail' on resource `arn:aws:ses:us-east-1:801906911770:identity/zachariahcrowell@gmail.com'"
---
+++ serverless-contact-form/handler.js
@@ -0,0 +1,54 @@
+// hander.js
+const AWS = require('aws-sdk');
+const SES = new AWS.SES();
+
+/* This tutorial was used for reference
+https://www.smashingmagazine.com/2018/05/building-serverless-contact-form-static-website/
+*/
+
+function sendEmail(formData, callback) {
+// Verify email address in aws console !
+
+/* If you are getting an "access denied" error in the console reference the below article to change lambda JSON
+https://stackoverflow.com/questions/34949316/access-denied-while-sending-email-from-aws-ses-in-lambda-function */
+
+  const emailParams = {
+    Source: 'zachariahcrowell@gmail.com', // SES SENDING EMAIL
+    ReplyToAddresses: [formData.reply_to],
+    Destination: {
+      ToAddresses: ['zachariahcrowell@gmail.com'], // SES RECEIVING EMAIL
+    },
+    Message: {
+      Body: {
+        Text: {
+          Charset: 'UTF-8',
+          Data: `${formData.message}\n\nName: ${formData.name}\nEmail: ${formData.reply_to}`,
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'New message from Zach_Crowell.com',
+      },
+    },
+  };
+
+  SES.sendEmail(emailParams, callback);
+}
+
+module.exports.staticSiteMailer = (event, context, callback) => {
+  const formData = JSON.parse(event.body);
+  sendEmail(formData, function(err, data) {
+    const response = {
+      statusCode: err ? 500 : 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        message: err ? err.message : data,
+      }),
+    };
+
+    callback(null, response);
+  });
+}
